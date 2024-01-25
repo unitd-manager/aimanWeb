5eaec941df42d7424d513df33024ae33e1d36e64
gokilaunitd <gokila@unitdtechnologies.com>
2024-01-25 14:01:33 +0530

    Website

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import {  Route, BrowserRouter as Router ,Routes} from 'react-router-dom';
 
 import Home from './pages/home';
+import Article from './pages/Article';
+// import Home from "../src/screens/pages/home/Home";
 import BlogDetails from './pages/BlogDetails';
 
 function App() {
@@ -12,6 +14,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/Article" element={<Article />} />
           {/* Add other routes as needed */}
           <Route path="/blog/:id" element={<BlogDetails />} />
         </Routes>
