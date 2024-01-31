f73408ee5bfc165670b92f01e68a3009e9334a98
Sulfiya786 <sulfiya@unitdtechnologies.com>
2024-01-31 13:23:27 +0530

    Membership Form

--- src/App.js
+++ src/App.js
@@ -8,16 +8,18 @@ import Article from './pages/Article';
 import BlogDetails from './pages/BlogDetails';
 import Gallery from './pages/Gallery';
 import Religious from './pages/Religious';
+import Membership from './pages/Membership';
 
 function App() {
   return (
     <div className="App">
-     
+
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/Article" element={<Article />} />
           <Route path="/Gallery" element={<Gallery />} />
+          <Route path="/Membership" element={<Membership />} />
           <Route path="/services/:title" element={<Religious />} />
           {/* Add other routes as needed */}
           <Route path="/blog/:id" element={<BlogDetails />} />
