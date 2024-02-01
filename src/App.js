068605d2c931229676ba6af4299ed49c021c70af
syed jasmine <smsaf91@gmail.com>
2024-02-01 11:40:56 +0530

    messag

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import './App.css';
 import {  Route, BrowserRouter as Router ,Routes} from 'react-router-dom';
 
 import Home from './pages/home';
-import Article from './pages/Article';
+import Articles from './pages/Articles';
 // import Home from "../src/screens/pages/home/Home";
 import BlogDetails from './pages/BlogDetails';
 import Gallery from './pages/Gallery';
 import News from './pages/News';
+import Events from './pages/Events';
+import NewsEdit from './pages/NewsEdit';
+import EventsEdit from './pages/EventsEdit';
 import Religious from './pages/Religious';
 
 function App() {
@@ -17,9 +20,12 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/Article" element={<Article />} />
+          <Route path="/Articles" element={<Articles />} />
           <Route path="/Gallery" element={<Gallery />} />
           <Route path="/News" element={<News />} />
+          <Route path="/Events" element={<Events />} />
+          <Route path="/NewsEdit/:id" element={<NewsEdit />} />
+          <Route path="/EventsEdit/:id" element={<EventsEdit />} />
           <Route path="/services/:title" element={<Religious />} />
           {/* Add other routes as needed */}
           <Route path="/blog/:id" element={<BlogDetails />} />
