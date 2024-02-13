c78204973d427aafeb997dcfce29d2df1d15a2cd
gokilaunitd <gokila@unitdtechnologies.com>
2024-02-13 11:44:25 +0530

    articles

--- src/pages/ArticlesPage.js
+++ src/pages/ArticlesPage.js
@@ -3,36 +3,24 @@ import { Button } from "reactstrap";
 import { useLocation, useParams } from "react-router-dom";
 import api from "../constants/api";
 import { Link } from "react-router-dom";
-import bannerImage from "../../src/assets/banner/home.jpg";
+import bannerImage from '../../src/assets/banner/home.jpg';
 //import NavMenu from "../components/NavMenu";
 import ReactHtmlParser from "react-html-parser";
 // import imageBase from "../../../constants/image.js";
 
 export default function ArticlesPage(props) {
   const [blogs, setBlogs] = useState([]);
-  //const [blogs1, setBlogs1] = useState([]);
   const [news, setNews] = useState([]);
   const [displayedNews, setDisplayedNews] = useState(3);
   const articlesPerPage = 3;
   const { title } = useParams();
 
- 
-
-  useEffect(() => {
-    if (title) {
-      getBlogs();
-    }
-  }, [title]);
-  
   const getBlogs = () => {
-    if (title) {
-      var formated = title.split("-").join(" ");
-      api.post("/content/getNewsandEvents", { title: formated }).then((res) => {
-        setBlogs(res.data.data);
-      });
-    }
+    var formated = title.split("-").join(" ");
+    api.post("/content/getNewsandEvents", { title: formated }).then((res) => {
+      setBlogs(res.data.data);
+    });
   };
-  
 
   //   const getArticlenews = () => {
   //   api.post("/getBlogTitle", { title: formated }).then((res) => {
@@ -57,7 +45,7 @@ export default function ArticlesPage(props) {
     var formatedd = title.toLowerCase();
     return formatedd.split(" ").join("-");
   };
-  console.log("blogs",blogs);
+  console.log(blogs);
   console.log(getFormatedText);
   const location = useLocation();
   console.log(props, " props");
@@ -94,14 +82,10 @@ export default function ArticlesPage(props) {
 
   return (
     <>
-      <div
-        class="breadcrumb portfolio-breadcrumb"
-        style={{
-          backgroundImage: `url(${bannerImage})`,
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-        }}
-      >
+      <div class="breadcrumb portfolio-breadcrumb" 
+      style={{backgroundImage: `url(${bannerImage})`,
+      backgroundSize: "cover",
+      backgroundPosition: "center" }}>
         <div class="container">
           <div class="row justify-content-center">
             <div class="col-xl-3 col-lg-3">
@@ -129,43 +113,41 @@ export default function ArticlesPage(props) {
           </div>
         ))}
       </div> */}
-{data.title}
       <div class="blog-inner">
         <div class="container">
           <div class="row justify-content-center">
             <div class="col-xl-8 col-lg-8">
               <div class="blog-details">
-                {blogs && blogs.map((data) => (
-                  <div key={data.id} className="blog-details">
-                    <div class="title">
-                      <h2>{data.title}</h2>
-                      <ul>
-                        <li>
-                          <span>
-                            <i class="flaticon-user"></i>
-                          </span>
-                          {data.modified_by}
-                        </li>
-                        <li>
-                          <span>
-                            <i class="flaticon-clock"></i>
-                          </span>
-                          {getFormattedDate(data.content_date)}
-                        </li>
-                      </ul>
-                    </div>
-                    <div class="main-img">
-                      <img
-                        src={`http://43.228.126.245/aimaanAPI/storage/uploads/${data.file_name}`}
-                        className="img-fluid card-img-top"
-                        alt="post-thumb"
-                      />
-                    </div>
-                    <div class="main-txt">
-                      <p>{ReactHtmlParser(data.description)}</p>
-                    </div>
+                <div key={data.id} class="blog-details">
+                  <div class="title">
+                    <h2>{data.title}</h2>
+                    <ul>
+                      <li>
+                        <span>
+                          <i class="flaticon-user"></i>
+                        </span>
+                        {data.modified_by}
+                      </li>
+                      <li>
+                        <span>
+                          <i class="flaticon-clock"></i>
+                        </span>
+                        {getFormattedDate(data.content_date)}
+                      </li>
+                    </ul>
+                  </div>
+                  <div class="main-img">
+                    <img
+                       src={`http://43.228.126.245/aimaanAPI/storage/uploads/${data.file_name}`}
+                      className="img-fluid card-img-top"
+                      alt="post-thumb"
+                    />
+                  </div>
+                  <div class="main-txt">
+                    <p>{ReactHtmlParser(data.description)}</p>
+                  </div>
 
-                    {/* <div class="other-option">
+                  {/* <div class="other-option">
                       <div class="share">
                         <h3>Share :</h3>
                         <a href="#" class="fb">
@@ -186,54 +168,51 @@ export default function ArticlesPage(props) {
                       </div>
                     </div> */}
 
-                    <div class="blog-nav">
-                      <div class="single-nav prev-blog">
-                        {/* <div class="img">
+                  <div class="blog-nav">
+                    <div class="single-nav prev-blog">
+                      {/* <div class="img">
                         <a href="blog-details.html">
                           <img
-                            src={`http://43.228.126.245/aimaanAPI/storage/uploads/${data.file_name}`}
+                            src={http://43.228.126.245/aimaanAPI/storage/uploads/${data.file_name}}
                             alt="image"
                           ></img>
                         </a>
                       </div> */}
 
-                        <div class="txt">
-                          {getNextArticle() && (
-                            <Link
-                              to={`/Articles/${getFormatedText(
-                                getNextArticle().title
-                              )}`}
-                              className="link"
-                              style={{ fontSize: "small" }}
-                            >
-                              {getNextArticle().title}
-                            </Link>
-                          )}
-                          <span style={{ color: "blue" }}>
-                            Next Article &gt;
-                          </span>
-                        </div>
+                      <div class="txt">
+                        {getNextArticle() && (
+                          <Link
+                            to={`/NewArticles/${getFormatedText(
+                              getNextArticle().title
+                            )}`}
+                            className="link"
+                            style={{ fontSize: 'small' }}
+                          >
+                            {getNextArticle().title}
+                          </Link>
+                        )}
+                        <span style={{ color: 'blue' }}>Next Article &gt;</span>
                       </div>
-                      <div class="single-nav next-blog">
-                        <div class="img">
-                          {getPreviousArticle() && (
-                            <Link
-                              to={`/Articles/${getFormatedText(
-                                getPreviousArticle().title
-                              )}`}
-                              className="link"
-                              style={{ fontSize: "small" }}
-                            >
-                              {getPreviousArticle().title}
-                            </Link>
-                          )}
-                        </div>
-                        <div class="txt">
-                          <span style={{ color: "blue" }}>
-                            &lt; Previous Article
-                          </span>
-                        </div>
-                        {/* 
+                    </div>
+                    <div class="single-nav next-blog">
+                      <div class="img">
+                        {getPreviousArticle() && (
+                          <Link
+                            to={`/NewArticles/${getFormatedText(
+                              getPreviousArticle().title
+                            )}`}
+                            className="link"
+                            style={{ fontSize: 'small' }}
+                          >
+                            {getPreviousArticle().title}
+                            
+                          </Link>
+                        )}
+                      </div>
+                      <div class="txt">
+                        <span style={{ color: 'blue' }}>&lt; Previous Article</span>
+                      </div>
+{/* 
                       <div class="img">
                         <a href="blog-details.html">
                           <img
@@ -242,10 +221,9 @@ export default function ArticlesPage(props) {
                           ></img>
                         </a>
                       </div> */}
-                      </div>
                     </div>
                   </div>
-                ))}
+                </div>
               </div>
             </div>
             <div class="col-xl-4 col-lg-4 col-md-6">
@@ -277,9 +255,8 @@ export default function ArticlesPage(props) {
                               className="link"
                             > */}
                             <Link
-                              to={`/Articles/${data.title}`}
+                              to={`/NewArticles/${data.title}`}
                               className="link"
-                              state={{ data: data }}
                             >
                               {" "}
                               <a href="#">{data.title.slice(0, 50)}</a>
@@ -299,7 +276,7 @@ export default function ArticlesPage(props) {
                                 "en-US"
                               )}
                             </span>
-                            {/* <Link to={`/NewArticles/${data.content_id}`}>
+                            {/* <Link to={/NewArticles/${data.content_id}}>
                               {" "}
                               Read More
                             </Link> */}
@@ -322,10 +299,9 @@ export default function ArticlesPage(props) {
                         {news &&
                           news.slice(0, 4).map((data) => (
                             <div key={data.id} class="col-md-6 mb-4">
-                              <span class="tag">{data.content_type}</span>
                               <div class="img">
                                 <img
-                                  src={`http://43.228.126.245/aimaanAPI/storage/uploads/${data.file_name}`}
+                                 src={`http://43.228.126.245/aimaanAPI/storage/uploads/${data.file_name}`}
                                   width="200px"
                                   height="60px"
                                   alt="post-thumb"
@@ -336,9 +312,8 @@ export default function ArticlesPage(props) {
                                   to={`/NewArticles/${data.title}`}
                                   className="link"
                                 >
-                                  <a href="#" style={{ fontSize: "small" }}>
-                                    {data.title.slice(0, 50)}
-                                  </a>
+                                 <a href="#" style={{ fontSize: 'small' }}>{data.title.slice(0, 50)}</a>
+
                                 </Link>
                               </div>
                             </div>
